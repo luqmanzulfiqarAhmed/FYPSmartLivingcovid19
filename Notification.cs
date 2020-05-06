9c7f093381f2f9d1d0e413de03692f67b2314c86
luqmanzulfiqarAhmed <40490385+luqmanzulfiqarAhmed@users.noreply.github.com>
2020-05-06 08:26:20 +0500

    notification

--- Notification.cs
+++ Notification.cs
@@ -2,11 +2,34 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+using MailKit.Net.Smtp;
+using MailKit;
+using MimeKit;
 namespace smartLiving
 {
     public class Notification
     {
         public Notification() { }
+
+
+        public bool sendEmail(){
+            var message=new MimeMessage();
+            message.From.Add(new MailboxAddress("Smart Living","smartliving321@gmail.com"));
+            message.To.Add(new MailboxAddress("Person","asadmirza547@gmail.com"));
+            message.Subject="API Message";
+            message.Body=new TextPart("plain"){
+                Text="Bund Dy Dy!!!"
+            };
+            using (var client=new SmtpClient()){
+                client.Connect("smtp.gmail.com",587,false);
+                client.Authenticate("smartliving321@gmail.com","living@smart1122");
+                client.Send(message);
+                client.Disconnect(true);
+//                client.ServerCertificateValidationCallback=();
+            };
+
+
+            return false;
+        }
     }
 }
