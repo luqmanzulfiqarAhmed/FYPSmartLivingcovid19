c06d4709f22cc59f0c25e412e9ec6ca548ddf713
luqman <f168210@nu.edu.pk>
2020-05-06 21:47:48 +0500

    Resident controller

--- Controllers/ResidentController.cs
+++ Controllers/ResidentController.cs
@@ -10,7 +10,7 @@ using smartLiving.Repostries;
 
 namespace smartLiving.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/Resident")]
     [ApiController]
     public class ResidentController : ControllerBase
     {
