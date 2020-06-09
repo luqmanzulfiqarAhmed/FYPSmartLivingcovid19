cf24c48e5f29587563da5b8dfc8b6b6802a5d5f8
luqman <f168210@nu.edu.pk>
2020-06-09 17:37:02 +0500

    post successfull db

--- MongoDb/MongoDbContext.cs
+++ MongoDb/MongoDbContext.cs
@@ -18,15 +18,18 @@ namespace smartLiving.MongoDb
 
             // var client = new MongoClient("mongodb://localhost:27017");
             // database = client.GetDatabase("HousingSocietyAppBuilder");
-string connectionString = 
-  @"mongodb://luqman:CDH0W3E6ZEpOyoCxvuo0LTjVKuKp8bekWCqQKMirwKc9ND7zhXBlmp5Ixxmia7gU05u7hhuKHu3hDiT7k4gvtA==@luqman.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&maxIdleTimeMS=120000&appName=@luqman@";
-MongoClientSettings settings = MongoClientSettings.FromUrl(
-  new MongoUrl(connectionString)
-);
-settings.SslSettings = 
-  new SslSettings() { EnabledSslProtocols = SslProtocols.Tls12 };
-var mongoClient = new MongoClient(settings);
-database = mongoClient.GetDatabase("HousingSocietyAppBuilder");
+
+            string connectionString =
+                @"mongodb://luqman:CDH0W3E6ZEpOyoCxvuo0LTjVKuKp8bekWCqQKMirwKc9ND7zhXBlmp5Ixxmia7gU05u7hhuKHu3hDiT7k4gvtA==@luqman.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false";
+  
+            MongoClientSettings settings = MongoClientSettings.FromUrl(
+              new MongoUrl(connectionString)
+            );
+            settings.SslSettings =
+              new SslSettings() { EnabledSslProtocols = SslProtocols.Tls12 };
+            var mongoClient = new MongoClient(settings);
+            database = mongoClient.GetDatabase("HousingSocietyAppBuilder");
+
         }
         public static MongoDbContext getMongoDbContext(IConfiguration config)
         {
